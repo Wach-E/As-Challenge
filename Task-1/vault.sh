0b1f0ce35cb02353045448b8a8d84287cec128ec
Ubuntu <ubuntu@ip-172-31-32-218.ap-southeast-1.compute.internal>
2022-07-27 03:00:31 +0000

    Added CSI for env

--- Task-1/vault.sh
+++ Task-1/vault.sh
@@ -6,10 +6,6 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
 helm install vault hashicorp/vault -n postgres
 
-# Set Kubernetes service account for vault
-kubectl create sa postgres-app -n postgres
-
-
 # Initialize and useal vault
 kubectl exec -ti vault-0 -n postgres -- vault operator init
 kubectl exec -ti vault-0 -n postgres -- vault operator unseal <Key1>
@@ -35,16 +31,20 @@ kubectl exec -ti vault-0 -n postgres -- vault kv put internal/database/config \
 kubectl exec -ti vault-0 -n postgres -- vault auth enable kubernetes
 
 # Setup Vault authorizations for Kubernetes 
-TOKEN_REVIEW_JWT=$(kubectl get secret postgres-app-secret -n postgres -o go-template='{{ .data.token }}' | base64 --decode)
+# old: TOKEN_REVIEW_JWT=$(kubectl get secret postgres-app-secret -n postgres -o go-template='{{ .data.token }}' | base64 --decode)
+TOKEN_REVIEW_JWT=$(kubectl get secret vault-auth -o go-template='{{ .data.token }}' | base64 --decode) 
 KUBE_CA_CERT=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 --decode)
 KUBE_HOST=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.server}')
+
+# REtriev cluster issuer
+kubectl proxy -n postgres & curl --silent http://127.0.0.1:8001/.well-known/openid-configuration | jq -r .issuer
 
 # Configure the Kubernetes authentication method to use the location of the Kubernetes API
 kubectl exec -ti vault-0 -n postgres -- vault write auth/kubernetes/config \
     kubernetes_host="$KUBE_HOST" \
     kubernetes_ca_cert="$KUBE_CA_CERT" \
-    token_reviewer_jwt="$TOKEN_REVIEW_JWT" \
-    disable_local_ca_jwt="true"
+    token_reviewer_jwt="$TOKEN_REVIEW_JWT" 
+    # issuer="https://kubernetes.default.svc.cluster.local"
     
 # Confirm vault authorization with Kubernetes
 kubectl exec -ti vault-0 -n postgres -- vault read auth/kubernetes/config
@@ -59,7 +59,16 @@ EOF
 
 # Create a Kubernetes authentication role named postgres-app
 kubectl exec -ti vault-0 -n postgres -- vault write auth/kubernetes/role/postgres-app \
-    bound_service_account_names=postgres-app \
-    bound_service_account_namespaces=postgres \
-    policies=postgres-app \
+    bound_service_account_names="postgres-app" \
+    bound_service_account_namespaces="postgres" \
+    policies="postgres-app" \
     ttl=24h
+    
+kubectl exec -ti vault-0 -n postgres -- vault read auth/kubernetes/role/postgres-app
+
+
+# Setup CSI
+helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+helm repo update
+helm install csi secrets-store-csi-driver/secrets-store-csi-driver \
+  --set syncSecret.enabled=true -n postgres
